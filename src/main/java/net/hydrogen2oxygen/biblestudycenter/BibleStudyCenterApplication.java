e0af9330c162c719a977c18a76ef7740b00e5eb2
hydrogen2oxygen <luxuspeter@googlemail.com>
2019-02-03 10:36:50 +0100

    Initial commit spring boot

---
+++ src/main/java/net/hydrogen2oxygen/biblestudycenter/BibleStudyCenterApplication.java
@@ -0,0 +1,14 @@
+package net.hydrogen2oxygen.biblestudycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleStudyCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleStudyCenterApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/net/hydrogen2oxygen/biblestudycenter/BibleStudyCenterApplicationTests.java
@@ -0,0 +1,17 @@
+package net.hydrogen2oxygen.biblestudycenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BibleStudyCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
