3ea316dc88d7cc76c8e3e221f927ff79ccb711f8
hydrogen2oxygen <luxuspeter@googlemail.com>
2019-02-04 16:49:08 +0100

    book, chapter, verse ... domain objects

---
+++ src/main/java/net/hydrogen2oxygen/biblestudycenter/domain/Chapter.java
@@ -0,0 +1,27 @@
+package net.hydrogen2oxygen.biblestudycenter.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chapter {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long bookId;
+    private Long number;
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "chapterId")
+    private List<Verse> verses = new ArrayList<>();
+}
---
+++ src/main/java/net/hydrogen2oxygen/biblestudycenter/domain/Verse.java
@@ -0,0 +1,24 @@
+package net.hydrogen2oxygen.biblestudycenter.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Verse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long chapterId;
+    private Long number;
+    private String description;
+}
